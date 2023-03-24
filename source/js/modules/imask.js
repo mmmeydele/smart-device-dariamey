0e97f990408081e53463f8ecce6581cb81216c27
Daria Mey <d.like.jagger@gmail.com>
2023-03-24 15:05:43 +0300

    Правки

--- source/js/modules/imask.js
+++ source/js/modules/imask.js
@@ -1,17 +1,17 @@
 import IMask from 'imask';
 
-let phoneInput = document.querySelector('.phone');
+let phoneInput = document.querySelector('.phone input[id="phone"]');
 
 // Создаем маску в инпуте
 const phoneMask = new IMask(phoneInput, {
   mask: '+{7}(000)000-00-00',
 });
 
-let modalPhone = document.querySelector('.modal__phone');
+let modalPhone = document.querySelector('.modal__phone input[id="call-phone"]');
 
 const modalMask = new IMask(modalPhone, {
   mask: '+{7}(000)000-00-00',
 });
 
 
-export {phoneMask, modalMask};
+export { phoneMask, modalMask };
